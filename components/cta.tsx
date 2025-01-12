8e5d84a04247f9b40c1d340202633e18232a02a5
Dubjay18 <jejeniyi7@gmail.com>
2025-01-13 00:02:32 +0100

    commit

--- components/cta.tsx
+++ components/cta.tsx
@@ -7,15 +7,15 @@ export function CTA() {
   return (
     <section className="py-24 bg-white">
       <div className="container mx-auto px-6">
-        <div className="bg-navy-900 px-8 py-12 relative rounded-lg mb-16">
+        <div className="bg-navy-900 px-8 py-12 relative rounded-lg overflow-hidden mb-16">
           <Image
             src={"/Ellipse.svg"}
             alt="ellipse"
             height={600}
             width={700}
-            className="absolute top-0 right-2"
+            className="absolute -top-72 right-0"
           />
-          <div className="max-w-3xl mx-auto text-center space-y-8">
+          <div className="max-w-3xl mx-auto text-center relative space-y-8">
             <H2 className="text-white">
               An enterprise template to ramp up your company website
             </H2>
